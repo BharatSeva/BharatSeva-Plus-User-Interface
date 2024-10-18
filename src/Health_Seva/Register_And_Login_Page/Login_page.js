b74539206f8b8ec0a1553ca945149a705ac75eba
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2024-10-18 14:40:22 +0530

    Improve the Login/Register Forms. Add tailwind css

--- src/Health_Seva/Register_And_Login_Page/Login_page.js
+++ src/Health_Seva/Register_And_Login_Page/Login_page.js
@@ -66,30 +66,71 @@ export default function LoginPage() {
                     <Navigate to="/user/dashboard" replace={true} />
                 </div>
             )}
-            <div className="RegisterContainer">
-                <div className="LoginContainer">
-                    <p className="BharatSeva_Registration BharatSevaLoginUser">BharatSeva+ User-InterFace</p >
-                    <h2 className="logintextcontainer">Login Here</h2>
-
-                    <form className="EnterDetails" onSubmit={preventDefault}>
-                        <label>Enter Your Health ID</label><br></br>
-                        <input type="Number" className="UserInputSectionLoginPage" placeholder="Enter Your Health ID" name="health_id" onChange={Credential} onKeyUp={Credential} required ></input><br></br>
-
-                        <label>Enter Your Password</label><br></br>
-                        <input type="password" className="UserInputSectionLoginPage" placeholder="Enter Your Password" name="password" onChange={Credential} onKeyUp={Credential} required ></input><br></br>
-
-                        <input type="submit" value={`${IsAuthenticated.IsFetching ? "Validating..." : "Login*"}`} disabled={IsAuthenticated.IsFetching} className="Submitbtn"></input>
-                        <p className="Mustberegisteredforlogin">*You Must Be Registered Before You Log-In!</p>
-                    </form>
 
 
-                    <div className="Login">
-                        <p className="RegisterLoginPage">Don't Have A Account ? <Link to="/user/register" className="RegisterBtn">Register Here</Link></p>
-                    </div>
-                        {/* Sign Up With Google */}
-                        <div className="GoogleOAuthButton"><p>Or</p><GoogleOAuthProvider clientId="476285565826-8smpt7q2bh9o1ace0iqn8lcmn52maele.apps.googleusercontent.com"><GoogleOAuth /></GoogleOAuthProvider></div>
+            {/* new login page design */}
+            <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+        <div className="max-w-md w-full">
+
+
+          <div className="p-8 rounded-2xl bg-white shadow">
+            <h2 className="text-gray-800 text-center text-2xl font-bold">LOGIN</h2>
+            <form className="mt-8 space-y-4" onSubmit={preventDefault}>
+              <div>
+                <label className="text-gray-800 text-sm mb-2 block">Health ID</label>
+                <div className="relative flex items-center">
+                  <input name="health_id" type="Number" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter Your Health ID" onChange={Credential} onKeyUp={Credential}/>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
+                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                    <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
+                  </svg>
                 </div>
+              </div>
+
+              <div>
+                <label className="text-gray-800 text-sm mb-2 block">Password</label>
+                <div className="relative flex items-center">
+                  <input name="password" type="password" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter Your Password" onChange={Credential} onKeyUp={Credential}/>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap items-center justify-between gap-4">
+                <div className="flex items-center">
+                  <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                  <label for="remember-me" className="ml-3 block text-sm text-gray-800">
+                    Remember me
+                  </label>
+                </div>
+                <div className="text-sm">
+                  <a href="jajvascript:void(0);" className="text-blue-600 hover:underline font-semibold">
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div className="!mt-8">
+                <button type="submit" className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none" value={`${IsAuthenticated.IsFetching ? "Validating..." : "Login*"}`} disabled={IsAuthenticated.IsFetching}>
+                  Sign in
+                </button>
+              </div>
+              <div class="my-4 flex items-center gap-4">
+              <hr class="w-full border-gray-300" />
+              <p class="text-sm text-gray-800 text-center">or</p>
+              <hr class="w-full border-gray-300" />
             </div>
+
+
+
+              <GoogleOAuthProvider clientId="476285565826-8smpt7q2bh9o1ace0iqn8lcmn52maele.apps.googleusercontent.com"><GoogleOAuth /></GoogleOAuthProvider>
+
+              <p className="text-gray-800 text-sm !mt-8 text-center">Don't have an account? <a href="/user/register" class="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Register here</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
 
             <div className="loginabouttextcontainer loginaboutwidht">
 
@@ -110,3 +151,32 @@ export default function LoginPage() {
     )
 }
 
+
+
+
+
+
+{/* <div className="RegisterContainer">
+                <div className="LoginContainer">
+                    <p className="BharatSeva_Registration BharatSevaLoginUser">BharatSeva+ User-InterFace</p >
+                    <h2 className="logintextcontainer">Login Here</h2>
+
+                    <form className="EnterDetails" onSubmit={preventDefault}>
+                        <label>Enter Your Health ID</label><br></br>
+                        <input type="Number" className="UserInputSectionLoginPage" placeholder="Enter Your Health ID" name="health_id" onChange={Credential} onKeyUp={Credential} required ></input><br></br>
+
+                        <label>Enter Your Password</label><br></br>
+                        <input type="password" className="UserInputSectionLoginPage" placeholder="Enter Your Password" name="password" onChange={Credential} onKeyUp={Credential} required ></input><br></br>
+
+                        <input type="submit" value={`${IsAuthenticated.IsFetching ? "Validating..." : "Login*"}`} disabled={IsAuthenticated.IsFetching} className="Submitbtn"></input>
+                        <p className="Mustberegisteredforlogin">*You Must Be Registered Before You Log-In!</p>
+                    </form>
+
+
+                    <div className="Login">
+                        <p className="RegisterLoginPage">Don't Have A Account ? <Link to="/user/register" className="RegisterBtn">Register Here</Link></p>
+                    </div>
+
+                        <div className="GoogleOAuthButton"><p>Or</p><GoogleOAuthProvider clientId="476285565826-8smpt7q2bh9o1ace0iqn8lcmn52maele.apps.googleusercontent.com"><GoogleOAuth /></GoogleOAuthProvider></div>
+                </div>
+            </div> */}
--- src/Health_Seva/Register_And_Login_Page/Register_Page.js
+++ src/Health_Seva/Register_And_Login_Page/Register_Page.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Message from "../Message";
 import "./Register_Page.css";
+import GoogleOAuth from "./GoogleAuth/GoogleOAuth"
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function RegisterPage() {
     document.title = "Register | Bharat Seva";
@@ -48,7 +50,7 @@ export default function RegisterPage() {
 
     const PasswordUser = document.querySelector("#PasswordUser")
     const PasswordAgainUser = document.querySelector("#AgainPasswordUser")
-    const PasswordNotmatch = document.querySelector(".PasswordDonotmatch")
+    const PasswordNotmatch = document.querySelector("#PasswordDonotmatch")
     const preventDefault = (e) => {
         SetIsregistered(false)
         e.preventDefault();
@@ -66,7 +68,7 @@ export default function RegisterPage() {
                     <Message message={IsRegistered} />
                 </div>
             )}
-            <div className="RegisterContainer">
+            {/* <div className="RegisterContainer">
                 <div className="Register">
                     <p className="BharatSeva_Registration">BharatSeva+ User Inter-Face</p >
                     <h2 className="Registerhealthuser">Register Yourself</h2>
@@ -90,7 +92,89 @@ export default function RegisterPage() {
                         <p className="RegisterLoginPage">Registered ?  <Link to="/user/login" className="LoginBtn">Login Here </Link> </p>
                     </div>
                 </div>
+            </div> */}
+
+
+<div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+        <div className="max-w-md w-full">
+
+
+          <div className="p-8 rounded-2xl bg-white shadow">
+            <h2 className="text-gray-800 text-center text-2xl font-bold">REGISTER</h2>
+            <form className="mt-8 space-y-4" onSubmit={preventDefault}>
+              <div>
+                <label className="text-gray-800 text-sm mb-2 block">Health ID</label>
+                <div className="relative flex items-center">
+                  <input name="health_id" id="UserRegiserhealthId" type="Number" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter Your Health ID" onChange={SetCredential} onKeyUp={SetCredential}/>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
+                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                    <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div>
+                <label className="text-gray-800 text-sm mb-2 block">Email</label>
+                <div className="relative flex items-center">
+                  <input name="email" type="email" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter Your Email" onChange={SetCredential} onKeyUp={SetCredential}/>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4" viewBox="0 0 682.667 682.667">
+                  <defs>
+                    <clipPath id="a" clipPathUnits="userSpaceOnUse">
+                      <path d="M0 512h512V0H0Z" data-original="#000000"></path>
+                    </clipPath>
+                  </defs>
+                  <g clip-path="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
+                    <path fill="none" stroke-miterlimit="10" stroke-width="40" d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z" data-original="#000000"></path>
+                    <path d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z" data-original="#000000"></path>
+                  </g>
+                </svg>
+                </div>
+              </div>
+
+              <div>
+                <label className="text-gray-800 text-sm mb-2 block">Password</label>
+                <div className="relative flex items-center">
+                  <input name="password" id="PasswordUser" type="password" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter Your Password" onChange={SetCredential} onKeyUp={SetCredential}/>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div>
+                <label className="text-gray-800 text-sm mb-2 block">Confirm Password</label>
+                <div className="relative flex items-center">
+                  <input name="password" id="AgainPasswordUser" type="password" required className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter Your Password Again" onChange={SetCredential} onKeyUp={SetCredential}/>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                  </svg>
+                </div>
+              </div>
+              <p id="PasswordDonotmatch">Password Do no Match</p>
+
+
+
+              <div className="!mt-8">
+                <button type="submit" className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none" value={`${IsLoading ? "Validating..." : "Register"}`} disabled={IsLoading}>
+                  Register
+                </button>
+              </div>
+              <div class="my-4 flex items-center gap-4">
+              <hr class="w-full border-gray-300" />
+              <p class="text-sm text-gray-800 text-center">or</p>
+              <hr class="w-full border-gray-300" />
             </div>
+
+
+
+              <GoogleOAuthProvider clientId="476285565826-8smpt7q2bh9o1ace0iqn8lcmn52maele.apps.googleusercontent.com"><GoogleOAuth /></GoogleOAuthProvider>
+
+              <p className="text-gray-800 text-sm !mt-8 text-center">Already registered? <a href="/user/login" class="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Login here</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
+
 
             <div className="Registertextcontaine loginabouttextcontainer">
                 <p>Points to Note</p>
